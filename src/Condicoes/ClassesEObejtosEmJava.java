08ca64f49d980774cda1c77792eb61c0f95109d0
Vanderson H Santos <bioolegari@gmail.com>
2025-02-09 21:27:10 -0300

    Classes e Objetos

--- src/Condicoes/ClassesEObejtosEmJava.java
+++
@@ -1,9 +0,0 @@
-package Condicoes;
-
-public class ClassesEObejtosEmJava {
-    public static void main(String[] args) {
-
-
-
-    }
-}
---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,25 @@
+package NivelIntermediario;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.idade = 20;
+        Naruto.aldeia = "Aldeia da Folha";
+
+
+        //Cria ninja Sasuke Uchiha
+
+        Ninja Sasuke = new Ninja();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.idade = 18;
+        Sasuke.aldeia = "Aldeia da Folha";
+
+        Ninja Sakura = new Ninja();
+        Sasuke.nome = "Sakura Haruno";
+        Sasuke.idade = 20;
+        Sasuke.aldeia = "Aldeia da Folha";
+
+    }
+}
